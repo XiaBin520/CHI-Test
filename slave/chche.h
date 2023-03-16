0d2b91b70ec019acdf690d9afb9d16e2096441ed
XiaBin520 <xiabin@shanghaitech.edu.cn>
2023-03-16 20:21:34 +0800

    cache: add CacheLine and Cache

---
+++ slave/chche.h
@@ -0,0 +1,44 @@
+#include<stdint.h>
+#include<string.h>
+
+enum{Owner_NONE = 0, Owner_LSREQ, Owner_CHIREQ};
+
+template<uint32_t size = 16>
+class CacheLine
+{
+public:
+    uint32_t    owner;
+    uint32_t    data[size]; // 32 bits * 16 = 4B * 16 = 64B = 512 bits
+
+    CacheLine()
+    {
+        owner = Owner_NONE;
+    }
+
+    void Write(uint32_t data[])
+    {
+        memcpy(this->data, data, sizeof(data));
+    }
+};
+
+
+template<uint32_t size = 1024>
+class Cache
+{
+private:
+    CacheLine* cache[size]; // 1024 * 64B = 64 KB
+
+public:
+    Cache()
+    {
+        for(uint32_t i = 0; i < size; i++)
+        {
+            CacheLine *cacheline = new CacheLine();
+        }
+    }
+
+    CacheLine* GetLine(uint64_t addr)
+    {
+        return cache[addr];
+    }
+};
